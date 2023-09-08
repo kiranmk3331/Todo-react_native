f154e16dd619d15e128f3b0eaf9b92f7b426d0f8
“kiranmk3331” <kiranmk3331@gmail.com>
2023-09-08 15:12:05 +0530

    Updated scroll with flatlist.

--- App.js
+++ App.js
@@ -1,7 +1,7 @@
 import { useState } from "react";
 import {
   Button,
-  ScrollView,
+  FlatList,
   StyleSheet,
   Text,
   TextInput,
@@ -19,7 +19,7 @@ export default function App() {
   const addGoalHandler = () => {
     setCourseGoals((currentGoalCourse) => [
       ...currentGoalCourse,
-      enteredGoalText,
+      { text: enteredGoalText, id: Math.random().toString() },
     ]);
     setEnteredGoalText("");
   };
@@ -36,13 +36,19 @@ export default function App() {
         <Button title="Add Goal" onPress={addGoalHandler} />
       </View>
       <View style={styles.goalsContainer}>
-        <ScrollView>
-          {courseGoals.map((goal) => (
-            <View style={styles.goalItem} key={goal}>
-              <Text style={styles.goalText}>{goal}</Text>
-            </View>
-          ))}
-        </ScrollView>
+        <FlatList
+          data={courseGoals}
+          renderItem={(itemData) => {
+            return (
+              <View style={styles.goalItem}>
+                <Text style={styles.goalText}>{itemData.item.text}</Text>
+              </View>
+            );
+          }}
+          keyExtractor={(item, index) => {
+            return item.id;
+          }}
+        />
       </View>
     </View>
   );
